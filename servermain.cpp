b3a1f4f92b3f60ca78d88345092ffe11b4fb9eaa
WilliamLebrato <william.lebrato@gmail.com>
2024-09-27 15:49:44 +0200

    TCP 1.0 and OK\n

--- servermain.cpp
+++ servermain.cpp
@@ -10,6 +10,7 @@
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
+#define BUFFER_SIZE 128
 
 
 using namespace std;
@@ -61,11 +62,52 @@ int main(int argc, char *argv[]){
     printf("Socket bound to port %d successfully.\n", port);
 
     // Step 4: Listen for incoming connections
-    if (listen(sockfd, 5) == -1) {
+    if (listen(sockfd, 5) < 0) {
         perror("Listen failed");
         close(sockfd);
         return -1;
     }
 
+    // Step 5: Accept a connection
+    struct sockaddr_in client_address;
+    socklen_t client_address_len = sizeof(client_address);
+    int client_sockfd = accept(sockfd, (struct sockaddr *)&client_address, &client_address_len);
+
+    if (client_sockfd < 0) {
+        perror("Accept failed");
+        close(sockfd);
+        return -1;
+    }
+
+    const char *protocol_msg = "TEXT TCP 1.0\n\n";
+    if (send(client_sockfd, protocol_msg, strlen(protocol_msg), 0) < 0) {
+        perror("Send failed");
+        close(client_sockfd);
+        close(sockfd);
+        return -1;
+}
+
+    // Read the response
+    char buf[BUFFER_SIZE];
+    memset(buf, 0, sizeof buf);
+    int numbytes;
+
+    if ((numbytes = recv(client_sockfd, buf, BUFFER_SIZE - 1, 0)) == -1) {
+        perror("recv");
+        close(client_sockfd);
+        close(sockfd);
+        return -1;
+    }
+
+    if (strstr(buf, "OK\n") == NULL) {
+        printf("ERROR: Expected 'OK\\n' response\n");
+        close(client_sockfd);
+        close(sockfd);
+        return -1;
+    }
+
     
+
+close(sockfd);
+return 0;
 }
