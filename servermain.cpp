00efe2d061181037d123404454054b17c4170c11
WilliamLebrato <william.lebrato@gmail.com>
2024-09-27 15:42:45 +0200

    Added The Socket And its listening

--- servermain.cpp
+++ servermain.cpp
@@ -2,10 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 /* You will to add includes here */
-
-
-// Included to get the support library
-#include <calcLib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
 
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
@@ -33,5 +33,39 @@ int main(int argc, char *argv[]){
   printf("Host %s, and port %d.\n",Desthost,port);
 #endif
 
+// Implement the socket
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("socket() failed");
+    return -1;
+  }
 
+  int opt = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) 
+  {
+    perror("setsockopt() failed");
+    return -1;
+  }
+
+    // Step 3: Bind the socket to an IP address and port
+    struct sockaddr_in address;
+    address.sin_family = AF_UNSPEC;
+    address.sin_addr.s_addr = INADDR_ANY; // Bind to all available interfaces
+    address.sin_port = htons(port); // Convert port number to network byte order
+
+    if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("Bind failed");
+        close(sockfd);
+        return -1;
+    }
+    printf("Socket bound to port %d successfully.\n", port);
+
+    // Step 4: Listen for incoming connections
+    if (listen(sockfd, 5) == -1) {
+        perror("Listen failed");
+        close(sockfd);
+        return -1;
+    }
+
+    
 }
